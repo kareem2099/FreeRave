3a10eb009ec08876036f273707cc9fbfa3b4166a
kareem2099 <kareem20990@yahoo.com>
2024-09-06 18:45:29 +0300

    update

---
+++ lib/main_screens/home_screen/public_chat/cubit/stream_chat_state.dart
@@ -0,0 +1,7 @@
+abstract class ChatState {}
+
+class ChatInitial extends ChatState {}
+
+class ChatConnected extends ChatState {}
+
+class ChatDisconnected extends ChatState {}
---
+++ lib/main_screens/profile/cubit/profile_state.dart
@@ -0,0 +1,23 @@
+
+import '../../main_screens/components/connections/models/user_model.dart';
+
+abstract class ProfileState {}
+
+class ProfileInitial extends ProfileState {}
+
+class ProfileLoading extends ProfileState {}
+
+class ProfileLoaded extends ProfileState {
+  final UserModel userModel;
+
+  ProfileLoaded(this.userModel);
+}
+
+class ProfileError extends ProfileState {
+  final String message;
+  ProfileError(this.message);
+}
+
+class ProfileUpdating extends ProfileState {}
+
+class ProfileUpdated extends ProfileState {}
